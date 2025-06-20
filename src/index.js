9cd1d1aa03a118973a27aa017ce07bc877f2aa85
Yousif-Hibi <95185875+Yousif-Hibi@users.noreply.github.com>
2025-06-20 18:10:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,6 @@ import App from './App';
 // Initialize Firebase
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBDxWaOapX03TXYUWDrxlJ3caiApknK3o0",
-  authDomain: "beekeeping-8f7e6.firebaseapp.com",
-  projectId: "beekeeping-8f7e6",
-  storageBucket: "beekeeping-8f7e6.appspot.com",
-  messagingSenderId: "43486883566",
-  appId: "1:43486883566:web:b512c45d93113b91548dff",
-  measurementId: "G-SZ0PL1ERZG"
-};
 
 initializeApp(firebaseConfig);
 
